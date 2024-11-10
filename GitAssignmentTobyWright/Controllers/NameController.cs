a31acb746c124993383d37a22c785203f2a45276
TobyW92 <tobywright@live.com>
2024-11-10 17:50:13 -0500

    Updated API to show name and github ID.

---
+++ GitAssignmentTobyWright/Controllers/NameController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GitAssignmentTobyWright.Models;
+
+namespace GitAssignmentTobyWright.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        private readonly ILogger<NameController> _logger;
+
+        public NameController(ILogger<NameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var person = new Name { NameFirst = "Toby", NameLast = "Wright", GithubID = "TobyW92" };
+            return Ok(person);
+        }
+
+    }
+}
---
+++ GitAssignmentTobyWright/Models/Name.cs
@@ -0,0 +1,11 @@
+﻿namespace GitAssignmentTobyWright.Models
+{
+    public class Name
+    {
+        public required string NameFirst { get; set; }
+
+        public required string NameLast { get; set; }
+
+        public required string GithubID { get; set; }
+    }
+}
